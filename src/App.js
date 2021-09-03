c7d675008583acd08524cec99a08a987ab463668
lucas <mr.pine97@gmail.com>
2021-09-03 12:44:44 -0300

    refact: transformando componente App em class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import data from './data';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <main className="movie-list">
-        <MovieList className="movie-list" movies={ data } />
-      </main>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Header />
+        <main className="movie-list">
+          <MovieList className="movie-list" movies={ data } />
+        </main>
+      </>
+    );
+  }
 }
 
 export default App;
